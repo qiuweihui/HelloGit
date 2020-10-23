836d41ca3385ad2b65996579e3ca6aba13f3fea8
qiuweihui <39258390+qiuweihui@users.noreply.github.com>
2020-10-23 10:50:57 +0800

    Update HelloGit.java

--- HelloGit/src/main/java/com/cug/git/HelloGit.java
+++ HelloGit/src/main/java/com/cug/git/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("更新1!");
         System.out.println("更新dev!");
+        System.out.println("在线更新!");
 
     }
 }
